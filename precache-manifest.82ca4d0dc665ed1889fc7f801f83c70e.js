6588fa3fe03a7be02ee1147f1d405940216784eb
jddelia <jddelia123@gmail.com>
2018-12-21 21:33:25 -0500

    Updates

---
+++ precache-manifest.82ca4d0dc665ed1889fc7f801f83c70e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9c65e677aa25357ccfd6d8f14984443",
+    "url": "/activate/static/media/activatelogo.a9c65e67.png"
+  },
+  {
+    "revision": "5bae620a6c904af6d87f7fd1e625db2b",
+    "url": "/activate/static/media/activate.5bae620a.png"
+  },
+  {
+    "revision": "a2944ac4b9e4beaa7ef6",
+    "url": "/activate/static/js/runtime~main.a2944ac4.js"
+  },
+  {
+    "revision": "4a3c5e619cec32a4d150",
+    "url": "/activate/static/js/main.4a3c5e61.chunk.js"
+  },
+  {
+    "revision": "49bee6f5641eddd5c488",
+    "url": "/activate/static/js/1.49bee6f5.chunk.js"
+  },
+  {
+    "revision": "4a3c5e619cec32a4d150",
+    "url": "/activate/static/css/main.80c9deb8.chunk.css"
+  },
+  {
+    "revision": "6e8b3d0ee31234cd2238ecca64439fb2",
+    "url": "/activate/index.html"
+  }
+];
